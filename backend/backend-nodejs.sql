3eb05db75e669ed2f7529afa4848dbc7e58fe5a5
Muhammad Fadilah <1119110006@global.ac.id>
2024-09-12 15:37:49 +0700

    database

--- backend/backend-nodejs.sql
+++ backend/backend-nodejs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Waktu pembuatan: 26 Agu 2024 pada 07.14
+-- Waktu pembuatan: 12 Sep 2024 pada 10.37
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.1.25
 
@@ -47,17 +47,9 @@ CREATE TABLE `karyawan` (
   `Skill` varchar(255) DEFAULT NULL,
   `Bersedia` varchar(255) DEFAULT NULL,
   `Penghasilan` varchar(255) DEFAULT NULL,
-  `refresh_token` text DEFAULT NULL,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data untuk tabel `karyawan`
---
-
-INSERT INTO `karyawan` (`id`, `Posisi`, `Nama`, `Ktp`, `Ttl`, `Jenkel`, `Agama`, `Gol`, `Status`, `AlamatKtp`, `AlamatTinggal`, `Email`, `Nohp`, `Pendidikan`, `Pelatihan`, `Pekerjaan`, `Skill`, `Bersedia`, `Penghasilan`, `refresh_token`, `createdAt`, `updatedAt`) VALUES
-(4, 'Backend Developer', 'Muhammad Fadilah', '3671080111990004', '1999-09-01 00:00:00', 'L', 'Islam', 'AB', 'Single', 'villa tangerang regency sangiang', 'villa tangerang regency sangiang', 'afadilah162@gmail.com', 2147483647, 'S1', 'Enigma Camp', 'Manager', 'ReactJS, NextJS, NuxtJS, ExpressJS, PostgreSQL, MYSQL,  Golang, NodeJS, C#, .NET,  Laravel, MongoDB, Echo, Gin, Docker, CI/CD, Git, Gitlab', 'Ya', '8.000.000', NULL, '2024-08-26 02:19:12', '2024-08-26 03:14:36');
 
 -- --------------------------------------------------------
 
@@ -69,6 +61,7 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
+  `role` varchar(255) DEFAULT NULL,
   `refresh_token` text DEFAULT NULL,
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL
@@ -78,9 +71,9 @@ CREATE TABLE `users` (
 -- Dumping data untuk tabel `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `password`, `refresh_token`, `createdAt`, `updatedAt`) VALUES
-(1, 'ahmad123@gmail.com', '$2b$10$bDc8NJwHVdrQodtOpWP3T.ll/yl2653k1NCd8mdyaJs.J3d3f35cG', NULL, '2024-08-25 06:05:41', '2024-08-26 04:33:04'),
-(22, 'joko@gmail.com', '$2b$10$O6I6J6YtZXBP5QzDVQcj2uMjuCQBXhUiId0xR3m8WZxKlmk12C2ZK', NULL, '2024-08-26 04:47:15', '2024-08-26 04:58:02');
+INSERT INTO `users` (`id`, `email`, `password`, `role`, `refresh_token`, `createdAt`, `updatedAt`) VALUES
+(1, 'admin@gmail.com', '$2b$10$aOx5B0ndKomv/xUBT2IQIuzFghTbA.sI9UwYS63SrFWXZfskyV7r6', 'admin', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoiYWRtaW5AZ21haWwuY29tIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzI2MTI4NzMzLCJleHAiOjE3MjYyMTUxMzN9.8OJltTosvyoZbu3QGK7EmeuVzJeKLNIs37Ufhar7OVw', '2024-09-12 08:12:02', '2024-09-12 08:12:13'),
+(2, 'user@gmail.com', '$2b$10$zZlnUWGIg.vfsA5EDl.MsuYEJTewoapu30fk.WMKNlSqe6gZB8xY6', 'user', NULL, '2024-09-12 08:12:27', '2024-09-12 08:12:27');
 
 --
 -- Indexes for dumped tables
@@ -106,13 +99,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT untuk tabel `karyawan`
 --
 ALTER TABLE `karyawan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
